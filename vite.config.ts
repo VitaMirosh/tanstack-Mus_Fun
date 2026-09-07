f61c569b676df7c1f79dd0449a6771a1eb7a0cb3
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-09-07 22:22:26 +0300

    create new project

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
+
+// https://vite.dev/config/
+export default defineConfig({
+
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+      routesDirectory: './src/app/routes',
+      generatedRouteTree: './src/app/routeTree.gen.ts',
+    }),
+    react()],
+})
