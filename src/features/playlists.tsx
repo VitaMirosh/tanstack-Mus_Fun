d95fe695c27f77fb3568ac67c17ddf357a7687fe
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-09-08 21:54:09 +0300

    refactor:add search in playlists,add useEffect to open window

--- src/features/playlists.tsx
+++ src/features/playlists.tsx
@@ -4,34 +4,40 @@ import {Pagination} from '../shared/ui/pagination/pagination.tsx';
 import {useState} from 'react';
 
 export const Playlists = () => {
-  const[page,setPage] = useState(1)
+  const [page, setPage] = useState(1)
+  const [search, setSearch] = useState('')
   const query = useQuery({
-    queryKey: ['playlists',page],
-    queryFn:async ({signal}) => {
-     const response = await client.GET('/playlists',{
-       params: {
-         query:{
-           pageNumber:page
-         }
-       },
-       signal
-     });
-     if(response.error){
-       throw (response as unknown as {error: Error}).error;
-     }
-     return response.data;
+    queryKey: ['playlists', {page, search}],
+    queryFn: async ({signal}) => {
+      const response = await client.GET('/playlists', {
+        params: {
+          query: {
+            pageNumber: page,
+            search
+          }
+        },
+        signal
+      });
+      if (response.error) {
+        throw (response as unknown as { error: Error }).error;
+      }
+      return response.data;
     },
-    placeholderData:keepPreviousData
+    placeholderData: keepPreviousData
   })
-  console.log('status:'+ query.status);
-  console.log('fetchStatus:'+ query.fetchStatus)
+  console.log('status:' + query.status);
+  console.log('fetchStatus:' + query.fetchStatus)
 
 
-  if(query.isPending) return <span> "🕥"</span>
-  if(query.isError)return <span>Error: {JSON.stringify(query.error.message)}</span>
+  if (query.isPending) return <span> "🕥"</span>
+  if (query.isError) return <span>Error: {JSON.stringify(query.error.message)}</span>
 
   return <div>
-  <Pagination pagesCount={query.data.meta.pagesCount} currentPage={page} onPageNumberChange={setPage} isFetching={query.isFetching}/>
+    <div>
+      <input value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search..."/>
+    </div>
+    <Pagination pagesCount={query.data.meta.pagesCount} currentPage={page} onPageNumberChange={setPage}
+                isFetching={query.isFetching}/>
     <ul>
       {query.data.data.map(playlist => (
         <li key={playlist.id}>
--- src/pages/auth/oath-callback-page.tsx
+++ src/pages/auth/oath-callback-page.tsx
@@ -1,7 +1,15 @@
+import {useEffect} from 'react';
 
 
 export function OathCallbackPage() {
-
+useEffect(() => {
+  const url = new URL(window.location.href);
+  const code = url.searchParams.get('code');
+  if(code && window.opener){
+    window.opener.postMessage({code},window.location.origin)
+  }
+  window.close();
+},[])
   return (
     <>
       <h2>OAuth Callback page </h2>
